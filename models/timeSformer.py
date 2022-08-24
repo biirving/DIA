600110760e9bebe54c002834670a3f94a94b7083
Irving <biyoirv@3c06301bd1c7.ant.amazon.com>
2022-08-24 11:01:45 -0700

    updated model, need to fix class token processing

--- models/timeSformer.py
+++ models/timeSformer.py
@@ -4,6 +4,7 @@ from torch import nn, tensor
 import torch
 import numpy as np
 from einops import rearrange, repeat
+from einops.layers.torch import Rearrange
 import math
 
 
@@ -56,65 +57,125 @@ class dividedSpaceTimeAttention(nn.Module):
             temporal = self.softmax(torch.matmul(q_mat[:, :, 0::self.n, :], torch.transpose(k_mat[:, :, 0::self.n, :], 2, 3)) / (math.sqrt(self.Dh) * self.num_heads))
             temporal = torch.matmul(temporal, v_mat[:, :, 0::self.n, :])
             temporal = torch.sum(temporal, 2, keepdim = True)
-           # print(temporal.shape)
-            temporal = temporal.repeat(1, 1, self.num_frames, 1)
-           # print(temporal.shape)
             
             # temporal calculation
             for x in range(1, self.n):
                 # get all of the patches at that frame
-                #print(torch.transpose(q_mat, 2 , 3).shape)
-                #inter = self.softmax(torch.matmul(torch.transpose(q_mat, 2 , 3), k_mat[:, :, x::self.n, :]) / (math.sqrt(self.Dh) * self.num_heads))
                 inter = self.softmax(torch.matmul(q_mat[:, :, x::self.n, :], torch.transpose(k_mat[:, :, x::self.n, :], 2, 3)) / (math.sqrt(self.Dh) * self.num_heads))
                 inter = torch.matmul(inter, v_mat[:, :, x::self.n, :])
-                #print(inter.shape)
                 inter = torch.sum(inter, 2, keepdim = True)
-                inter = inter.repeat(1, 1, self.num_frames, 1)
+                #inter = inter.repeat(1, 1, self.num_frames, 1)
                 temporal = torch.cat((temporal, inter), 2)
             
-            print(temporal.shape)
+            temporal = temporal.repeat(1, 1, self.num_frames, 1)
             temporal_input = self.multimad_temp(rearrange(temporal, 'b h nf d -> b nf (h d)', h = self.num_heads))
-            print(temporal_input.shape)
 
             q_mat = rearrange(self.q(temporal_input), 'b nf (h d) -> b h nf d', h = self.num_heads)
             v_mat = rearrange(self.k(temporal_input), 'b nf (h d) -> b h nf d', h = self.num_heads)
             k_mat = rearrange(self.v(temporal_input), 'b nf (h d) -> b h nf d', h = self.num_heads)
 
-            temporal = self.softmax(torch.matmul(q_mat[:, :, 0::self.n, :], torch.transpose(k_mat[:, :, 0::self.n, :], 2, 3)) / (math.sqrt(self.Dh) * self.num_heads))
-            temporal = torch.matmul(temporal, v_mat[:, :, 0::self.n, :])
+            temporal = self.softmax(torch.matmul(q_mat[:, :, 0:self.n, :], torch.transpose(k_mat[:, :, 0:self.n, :], 2, 3)) / (math.sqrt(self.Dh) * self.num_heads))
+            temporal = torch.matmul(temporal, v_mat[:, :, 0:self.n, :])
             temporal = torch.sum(temporal, 2, keepdim = True)
-           # print(temporal.shape)
-          #  temporal = temporal.repeat(1, 1, self.num_frames, 1)
-           # print(temporal.shape)
-            
+
+
             # spatial calculation
-            for x in range(1, self.n):
-                # get all of the patches at that frame
-                #print(torch.transpose(q_mat, 2 , 3).shape)
-                #inter = self.softmax(torch.matmul(torch.transpose(q_mat, 2 , 3), k_mat[:, :, x::self.n, :]) / (math.sqrt(self.Dh) * self.num_heads))
-                inter = self.softmax(torch.matmul(q_mat[:, :, x::self.n, :], torch.transpose(k_mat[:, :, x::self.n, :], 2, 3)) / (math.sqrt(self.Dh) * self.num_heads))
+            for x in range(1, self.num_frames):
+                inter = self.softmax(torch.matmul(q_mat[:, :, x:x+self.n, :], torch.transpose(k_mat[:, :, x::self.n, :], 2, 3)) / (math.sqrt(self.Dh) * self.num_heads))
                 inter = torch.matmul(inter, v_mat[:, :, x::self.n, :])
-                #print(inter.shape)
+                print(inter.shape)
                 inter = torch.sum(inter, 2, keepdim = True)
-            #    inter = inter.repeat(1, 1, self.num_frames, 1)
                 temporal = torch.cat((temporal, inter), 2)
 
-            print(temporal.shape)
+            temporal = temporal.repeat(1, 1, self.n, 1)
             output = self.multi_mad_final(rearrange(temporal, 'b h nf d -> b nf (h d)', h = self.num_heads))
-            print(output.shape)
-            print(output)
-            return output.repeat(1, self.num_frames, 1)
-          
+            return output
+            
         
+
+class EncoderBlock(nn.Module):
+    def __init__(self, num_heads, dim, n, num_frames):
+        super(EncoderBlock, self).__init__()
+        # number of attention heads
+        self.num_heads = num_heads
+        # number of attention blocks
+        self.dim = int(dim)
+        self.num_frames = num_frames
+        # layer normalization, to stabilize the gradients
+        # should not depend on batch size
+        self.n = int(n)
+        #self.norm = nn.LayerNorm(normalized_shape = (self.n, self.dim))
+        self.norm = nn.LayerNorm(normalized_shape = (self.num_frames * self.n + 1, dim), elementwise_affine = True)
+        self.attention = dividedSpaceTimeAttention(self.num_heads, dim, self.n, num_frames)
+        self.dh = int(self.dim / self.num_heads)
+        self.mlp = nn.Linear(dim, dim)
+
+        
+    def forward(self, input):
+        whoa = self.norm(input)
+        uhOh = self.attention.forward(whoa)
+        uhHuh = self.norm(uhOh + input)
+        toAdd = uhOh + input    
+        output = self.mlp(uhHuh)
+        output += toAdd
+        return output
+
+
+# You should be able to feed an input of any batch size to the model
+class timeSformer(nn.Module):
+
+    def __init__(self, height, width, num_frames, patch_res, dim, num_classes, batch_size):
+        super(timeSformer, self).__init__()
+        self.checkPass = True
+        self.height = height
+        self.width = width
+        self.channels = 3
+        self.num_classes = num_classes
+        self.patch_res = patch_res 
+        self.dim = int(dim)
+        self.patch_dim = self.channels * patch_res * patch_res
+        self.n = int((height * width) / (patch_res ** 2))
+
+        # the patch embeddings for the vision transformer
+        # the 'height','width','channel', and 'batch' are gleaned from the shape of the tensor
+       # self.patchEmbed = nn.Sequential(
+        #    (rearrange(video, 'b f c (h p1) (w p2) -> b (f h w) (p1 p2 c)', p1 = p, p2 = p),
+         #   nn.Linear(self.patch_dim, dim)))
+
+        self.patchEmbed = nn.Sequential(
+            Rearrange('b f c (h p1) (w p2) -> b (f h w) (p1 p2 c)', p1 = patch_res, p2 = patch_res),
+            nn.Linear(self.patch_dim, dim),)
+        # the class token serves as a representation of the entire sequence
+        self.classtkn = nn.Parameter(torch.randn(batch_size, 1, dim))
+        # this will be concated to the end of the input before positional embedding
+        # should the class token be randomonly initialized? or the same across batches? See what happens during training
+
+        # the positional embedding should be applied based on what 
+        self.pos_embed = nn.Parameter(torch.randn(batch_size, num_frames * (self.n) +1, dim))
+        self.encoderBlocks = nn.ModuleList([EncoderBlock(num_heads = 8, dim = dim, n = self.n, num_frames=num_frames) for i in range(8)])
+
+        
+        self.mlpHead = nn.Sequential(nn.LayerNorm(dim), nn.GELU(), nn.Linear(self.dim, num_classes))
+        self.dropout = nn.Dropout(0.1)
+
+
+            
+
+    def forward(self, vid):
+        input = self.patchEmbed(vid)
+        input = torch.cat((input, self.classtkn), dim = 1)
+        input += self.pos_embed
+        input = self.dropout(input)
+        for encoder in self.encoderBlocks:
+            output = encoder.forward(input)
+            input = output
+        out = self.mlpHead(output[:, 0])
+        return out
 
 
 video = torch.randn(1, 5, 3, 224, 224)
 p = 16
-video = rearrange(video, 'b f c (h p1) (w p2) -> b (f h w) (p1 p2 c)', p1 = p, p2 = p)
-patch_embed = nn.Linear(768, 16)
-video_to_feed = patch_embed(video)
 n = 224 * 224 / (16 * 16)
-print(n)
-print(video_to_feed.shape)
-new = dividedSpaceTimeAttention(8, 16, n, 5)
-print(new(video_to_feed))
+new = timeSformer(224, 224, 5, 16, 32, 5, 1)
+print(new(video))
+
