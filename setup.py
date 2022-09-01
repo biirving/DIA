352228bbb265dba04af8e52a946a7f9d81b56b14
Lysander.curiosum <67127172+Lysander-curiosum@users.noreply.github.com>
2022-09-01 11:55:21 -0700

    requirements, setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name = "dia", 
+    version = '0.0.1',
+    author = 'Ben Irving',
+    author_email = 'irving.b@northeastern.edu',
+    description = 'A vision transformer library',
+    url = 'https://github.com/Lysander-curiosum/DIA.git', 
+    packages = ['DIA'], 
+)
+
