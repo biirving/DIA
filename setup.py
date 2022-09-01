5783a9b9a336492f946e28dcefade066a8f6b2b1
Lysander.curiosum <67127172+Lysander-curiosum@users.noreply.github.com>
2022-09-01 12:31:55 -0700

    new name

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name = 'dia', 
+    name = 'DecepticonsInAlexandria', 
     version = '0.0.1',
     author = 'Ben Irving',
     author_email = 'irving.b@northeastern.edu',
