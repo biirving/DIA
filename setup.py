ff59b5d99830d33aa3994f057512bea37c90e9c6
Lysander.curiosum <67127172+Lysander-curiosum@users.noreply.github.com>
2022-09-01 12:11:16 -0700

    what

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name = 'dia', 
+    name = 'DiA', 
     version = '0.0.1',
     author = 'Ben Irving',
     author_email = 'irving.b@northeastern.edu',
     description = 'A vision transformer library',
-    url = 'https://github.com/Lysander-curiosum/DIA.git',
+    url = 'https://github.com/Lysander-curiosum/DIA',
     package_dir = {"": "src"},
     packages = find_packages("src"), 
 )
